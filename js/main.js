ce14e41c8a5654e54578a86630326abd32384d77
claudia-romano <claudia.romano06@gmail.com>
2015-05-28 14:10:53 +0100

    autoplay implemented

--- js/main.js
+++ js/main.js
@@ -1,58 +1,94 @@
 jQuery(document).ready(function($){
-	//on mobile - open/close primary navigation clicking/tapping the menu icon
-	$('.cd-primary-nav').on('click', function(event){
-		if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
-	});
+	var slidesWrapper = $('.cd-hero-slider');
 
-	//upload videos if not on mobile
-	uploadVideo($('.cd-hero-slider'));
+	//check if a .cd-hero-slider exists in the DOM 
+	if ( slidesWrapper.length > 0 ) {
+		var primaryNav = $('.cd-primary-nav'),
+			sliderNav = $('.cd-slider-nav'),
+			navigationMarker = $('.cd-marker'),
+			slidesNumber = slidesWrapper.children('li').length,
+			visibleSlidePosition = 0,
+			autoPlayId,
+			autoPlayDelay = 5000;
 
-	//change visible slide
-	$('.cd-slider-nav li').on('click', function(event){
-		event.preventDefault();
-		var selectedItem = $(this);
-		if(!selectedItem.hasClass('selected')) {
-			// if it's not already selected
-			var selectedPosition = selectedItem.index(),
-				activePosition = $('.cd-hero-slider .selected').index();
-			if( activePosition < selectedPosition) {
-				nextSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
-			} else {
-				prevSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
-			}
+		//upload videos (if not on mobile devices)
+		uploadVideo(slidesWrapper);
 
-			updateNavigationMarker(selectedPosition+1);
-		}
-	});
+		//autoplay slider
+		setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
 
-	function nextSlide(container, pagination, n){
-		var visibleSlide = container.find('.selected'),
-			navigationDot = pagination.find('.selected');
+		//on mobile - open/close primary navigation clicking/tapping the menu icon
+		primaryNav.on('click', function(event){
+			if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
+		});
 		
+		//change visible slide
+		sliderNav.on('click', 'li', function(event){
+			event.preventDefault();
+			var selectedItem = $(this);
+			if(!selectedItem.hasClass('selected')) {
+				// if it's not already selected
+				var selectedPosition = selectedItem.index(),
+					activePosition = slidesWrapper.find('li.selected').index();
+				
+				if( activePosition < selectedPosition) {
+					nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
+				} else {
+					prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
+				}
+
+				//this is used for the autoplay
+				visibleSlidePosition = selectedPosition;
+
+				updateSliderNavigation(sliderNav, selectedPosition);
+				updateNavigationMarker(navigationMarker, selectedPosition+1);
+				//reset autoplay
+				setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
+			}
+		});
+	}
+
+	function nextSlide(visibleSlide, container, pagination, n){
 		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
 			visibleSlide.removeClass('is-moving');
 		});
-		
-		container.children('li').eq(n).addClass('selected from-right').prevAll().addClass('move-left');
-		navigationDot.removeClass('selected')
-		pagination.find('li').eq(n).addClass('selected');
 
+		container.children('li').eq(n).addClass('selected from-right').prevAll().addClass('move-left');
 		checkVideo(visibleSlide, container, n);
 	}
 
-	function prevSlide(container, pagination, n){
-		var visibleSlide = container.find('.selected'),
-			navigationDot = pagination.find('.selected');
-		
+	function prevSlide(visibleSlide, container, pagination, n){
 		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
 			visibleSlide.removeClass('is-moving');
 		});
 
 		container.children('li').eq(n).addClass('selected from-left').removeClass('move-left').nextAll().removeClass('move-left');
+		checkVideo(visibleSlide, container, n);
+	}
+
+	function updateSliderNavigation(pagination, n) {
+		var navigationDot = pagination.find('.selected');
 		navigationDot.removeClass('selected');
 		pagination.find('li').eq(n).addClass('selected');
+	}
 
-		checkVideo(visibleSlide, container, n);
+	function setAutoplay(wrapper, length, delay) {
+		if(wrapper.hasClass('autoplay')) {
+			clearInterval(autoPlayId);
+			autoPlayId = window.setInterval(function(){autoplaySlider(length)}, delay);
+		}
+	}
+
+	function autoplaySlider(length) {
+		if( visibleSlidePosition < length - 1) {
+			nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, visibleSlidePosition + 1);
+			visibleSlidePosition +=1;
+		} else {
+			prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, 0);
+			visibleSlidePosition = 0;
+		}
+		updateNavigationMarker(navigationMarker, visibleSlidePosition+1);
+		updateSliderNavigation(sliderNav, visibleSlidePosition);
 	}
 
 	function uploadVideo(container) {
@@ -63,22 +99,24 @@ jQuery(document).ready(function($){
 				var	videoUrl = videoWrapper.data('video'),
 					video = $('<video loop><source src="'+videoUrl+'.mp4" type="video/mp4" /><source src="'+videoUrl+'.webm" type="video/webm" /></video>');
 				video.appendTo(videoWrapper);
-
-				if(videoWrapper.parents('.cd-bg-video.selected').length > 0) video.get(0).play();
+				// play video if first slide
+				if(videoWrapper.parent('.cd-bg-video.selected').length > 0) video.get(0).play();
 			}
 		});
 	}
 
 	function checkVideo(hiddenSlide, container, n) {
 		//check if a video outside the viewport is playing - if yes, pause it
-		if( hiddenSlide.find('video').length > 0 ) hiddenSlide.find('video').get(0).pause();
+		var hiddenVideo = hiddenSlide.find('video');
+		if( hiddenVideo.length > 0 ) hiddenVideo.get(0).pause();
 
 		//check if the select slide contains a video element - if yes, play the video
-		if( container.children('li').eq(n).find('video').length > 0 ) container.children('li').eq(n).find('video').get(0).play();
+		var visibleVideo = container.children('li').eq(n).find('video');
+		if( visibleVideo.length > 0 ) visibleVideo.get(0).play();
 	}
 
-	function updateNavigationMarker(n) {
-		$('.cd-marker').removeClassPrefix('item').addClass('item-'+n);
+	function updateNavigationMarker(marker, n) {
+		marker.removeClassPrefix('item').addClass('item-'+n);
 	}
 
 	$.fn.removeClassPrefix = function(prefix) {
