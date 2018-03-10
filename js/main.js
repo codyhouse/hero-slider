0ec5b61c5c7fe628d244e9aadd881197f2abe064
claudia-romano <claudia.romano06@gmail.com>
2018-03-10 15:50:40 +0000

    Remove bourbon dependency
    - Remove bourbon dependency
    - Improvements to css/js
    - Fix bug on Firefox 58

--- js/main.js
+++ js/main.js
@@ -1,132 +1,177 @@
-jQuery(document).ready(function($){
-	var slidesWrapper = $('.cd-hero-slider');
+(function(){
+	//remove no-js class
+	removeClass(document.getElementsByTagName("html")[0], "no-js");
 
-	//check if a .cd-hero-slider exists in the DOM 
-	if ( slidesWrapper.length > 0 ) {
-		var primaryNav = $('.cd-primary-nav'),
-			sliderNav = $('.cd-slider-nav'),
-			navigationMarker = $('.cd-marker'),
-			slidesNumber = slidesWrapper.children('li').length,
-			visibleSlidePosition = 0,
-			autoPlayId,
-			autoPlayDelay = 5000;
+	function HeroSlider( element ) {
+		this.element = element;
+		this.navigation = this.element.getElementsByClassName("js-cd-nav")[0];
+		this.navigationItems = this.navigation.getElementsByTagName('li');
+		this.marker = this.navigation.getElementsByClassName("js-cd-marker")[0];
+		this.slides = this.element.getElementsByClassName("js-cd-slide");
+		this.slidesNumber = this.slides.length;
+		this.newSlideIndex = 0;
+		this.oldSlideIndex = 0;
+		this.autoplay = hasClass(this.element, "js-cd-autoplay");
+		this.autoPlayId;
+		this.autoPlayDelay = 5000;
+		this.init();
+	};
 
-		//upload videos (if not on mobile devices)
-		uploadVideo(slidesWrapper);
-
+	HeroSlider.prototype.init = function() {
+		var self = this;
+		//upload video (if not on mobile devices)
+		this.uploadVideo();
 		//autoplay slider
-		setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
-
-		//on mobile - open/close primary navigation clicking/tapping the menu icon
-		primaryNav.on('click', function(event){
-			if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
-		});
-		
-		//change visible slide
-		sliderNav.on('click', 'li', function(event){
+		this.setAutoplay();
+		//listen for the click event on the slider navigation
+		this.navigation.addEventListener('click', function(event){
+			if( event.target.tagName.toLowerCase() == 'div' )
+				return;
 			event.preventDefault();
-			var selectedItem = $(this);
-			if(!selectedItem.hasClass('selected')) {
-				// if it's not already selected
-				var selectedPosition = selectedItem.index(),
-					activePosition = slidesWrapper.find('li.selected').index();
-				
-				if( activePosition < selectedPosition) {
-					nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
-				} else {
-					prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
-				}
-
-				//this is used for the autoplay
-				visibleSlidePosition = selectedPosition;
-
-				updateSliderNavigation(sliderNav, selectedPosition);
-				updateNavigationMarker(navigationMarker, selectedPosition+1);
-				//reset autoplay
-				setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
-			}
-		});
-	}
-
-	function nextSlide(visibleSlide, container, pagination, n){
-		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			visibleSlide.removeClass('is-moving');
+			var selectedSlide = event.target;
+			if( hasClass(event.target.parentElement, 'cd-selected') )
+				return;
+			self.oldSlideIndex = self.newSlideIndex;
+			self.newSlideIndex = Array.prototype.indexOf.call(self.navigationItems, event.target.parentElement);
+			self.newSlide();
+			self.updateNavigationMarker();
+			self.updateSliderNavigation();
+			self.setAutoplay();
 		});
 
-		container.children('li').eq(n).addClass('selected from-right').prevAll().addClass('move-left');
-		checkVideo(visibleSlide, container, n);
-	}
-
-	function prevSlide(visibleSlide, container, pagination, n){
-		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			visibleSlide.removeClass('is-moving');
-		});
-
-		container.children('li').eq(n).addClass('selected from-left').removeClass('move-left').nextAll().removeClass('move-left');
-		checkVideo(visibleSlide, container, n);
-	}
-
-	function updateSliderNavigation(pagination, n) {
-		var navigationDot = pagination.find('.selected');
-		navigationDot.removeClass('selected');
-		pagination.find('li').eq(n).addClass('selected');
-	}
-
-	function setAutoplay(wrapper, length, delay) {
-		if(wrapper.hasClass('autoplay')) {
-			clearInterval(autoPlayId);
-			autoPlayId = window.setInterval(function(){autoplaySlider(length)}, delay);
+		if(this.autoplay) {
+			// on hover - pause autoplay
+			this.element.addEventListener("mouseenter", function(){
+				clearInterval(self.autoPlayId);
+			});
+			this.element.addEventListener("mouseleave", function(){
+				self.setAutoplay();
+			});
 		}
-	}
+	};
 
-	function autoplaySlider(length) {
-		if( visibleSlidePosition < length - 1) {
-			nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, visibleSlidePosition + 1);
-			visibleSlidePosition +=1;
-		} else {
-			prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, 0);
-			visibleSlidePosition = 0;
-		}
-		updateNavigationMarker(navigationMarker, visibleSlidePosition+1);
-		updateSliderNavigation(sliderNav, visibleSlidePosition);
-	}
-
-	function uploadVideo(container) {
-		container.find('.cd-bg-video-wrapper').each(function(){
-			var videoWrapper = $(this);
-			if( videoWrapper.is(':visible') ) {
+	HeroSlider.prototype.uploadVideo = function() {
+		var videoSlides = this.element.getElementsByClassName("js-cd-bg-video");
+		for( var i = 0; i < videoSlides.length; i++) {
+			if( videoSlides[i].offsetHeight > 0 ) {
 				// if visible - we are not on a mobile device 
-				var	videoUrl = videoWrapper.data('video'),
-					video = $('<video loop><source src="'+videoUrl+'.mp4" type="video/mp4" /><source src="'+videoUrl+'.webm" type="video/webm" /></video>');
-				video.appendTo(videoWrapper);
-				// play video if first slide
-				if(videoWrapper.parent('.cd-bg-video.selected').length > 0) video.get(0).play();
+				var videoUrl = videoSlides[i].getAttribute("data-video");
+				videoSlides[i].innerHTML = "<video loop><source src='"+videoUrl+".mp4' type='video/mp4' /><source src='"+videoUrl+".webm' type='video/webm'/></video>";
+				// if the visible slide has a video - play it
+				if( hasClass(videoSlides[i].parentElement, "cd-hero__slide--selected") ) videoSlides[i].getElementsByTagName("video")[0].play();
 			}
-		});
-	}
+		}
+	};
 
-	function checkVideo(hiddenSlide, container, n) {
+	HeroSlider.prototype.setAutoplay = function() {
+		var self = this;
+		if(this.autoplay) {
+			clearInterval(self.autoPlayId);
+			self.autoPlayId = window.setInterval(function(){self.autoplaySlider()}, self.autoPlayDelay);
+		}
+	};
+
+	HeroSlider.prototype.autoplaySlider = function() {
+		this.oldSlideIndex = this.newSlideIndex;
+		var self = this;
+		if( this.newSlideIndex < this.slidesNumber - 1) {
+			this.newSlideIndex +=1;
+			this.newSlide();
+			
+		} else {
+			this.newSlideIndex = 0;
+			this.newSlide();
+		}
+
+		this.updateNavigationMarker();
+		this.updateSliderNavigation();
+	};
+
+	HeroSlider.prototype.newSlide = function(direction) {
+		var self = this;
+		removeClass(this.slides[this.oldSlideIndex], "cd-hero__slide--selected cd-hero__slide--from-left cd-hero__slide--from-right");
+		addClass(this.slides[this.oldSlideIndex], "cd-hero__slide--is-moving");
+		setTimeout(function(){removeClass(self.slides[self.oldSlideIndex], "cd-hero__slide--is-moving");}, 500);
+
+		for(var i=0; i < this.slidesNumber; i++) {
+			if( i < this.newSlideIndex && this.oldSlideIndex < this.newSlideIndex) {
+				addClass(this.slides[i], "cd-hero__slide--move-left");
+			} else if( i == this.newSlideIndex && this.oldSlideIndex < this.newSlideIndex) {
+				addClass(this.slides[i], "cd-hero__slide--selected cd-hero__slide--from-right");
+			} else if(i == this.newSlideIndex && this.oldSlideIndex > this.newSlideIndex) {
+				addClass(this.slides[i], "cd-hero__slide--selected cd-hero__slide--from-left");
+				removeClass(this.slides[i], "cd-hero__slide--move-left");
+			} else if( i > this.newSlideIndex && this.oldSlideIndex > this.newSlideIndex ) {
+				removeClass(this.slides[i], "cd-hero__slide--move-left");
+			}
+		}
+
+		this.checkVideo();
+
+	};
+
+	HeroSlider.prototype.updateNavigationMarker = function() {
+		removeClassPrefix(this.marker, 'item');
+		addClass(this.marker, "cd-hero__marker--item-"+ (Number(this.newSlideIndex) + 1));
+	};
+
+	HeroSlider.prototype.updateSliderNavigation = function() {
+		removeClass(this.navigationItems[this.oldSlideIndex], 'cd-selected');
+		addClass(this.navigationItems[this.newSlideIndex], 'cd-selected');
+	};
+
+	HeroSlider.prototype.checkVideo = function() {
 		//check if a video outside the viewport is playing - if yes, pause it
-		var hiddenVideo = hiddenSlide.find('video');
-		if( hiddenVideo.length > 0 ) hiddenVideo.get(0).pause();
+		var hiddenVideo = this.slides[this.oldSlideIndex].getElementsByTagName('video');
+		if( hiddenVideo.length ) hiddenVideo[0].pause();
 
 		//check if the select slide contains a video element - if yes, play the video
-		var visibleVideo = container.children('li').eq(n).find('video');
-		if( visibleVideo.length > 0 ) visibleVideo.get(0).play();
-	}
-
-	function updateNavigationMarker(marker, n) {
-		marker.removeClassPrefix('item').addClass('item-'+n);
-	}
-
-	$.fn.removeClassPrefix = function(prefix) {
-		//remove all classes starting with 'prefix'
-	    this.each(function(i, el) {
-	        var classes = el.className.split(" ").filter(function(c) {
-	            return c.lastIndexOf(prefix, 0) !== 0;
-	        });
-	        el.className = $.trim(classes.join(" "));
-	    });
-	    return this;
+		var visibleVideo = this.slides[this.newSlideIndex].getElementsByTagName('video');
+		if( visibleVideo.length ) visibleVideo[0].play();
 	};
-});
+
+	var heroSliders = document.getElementsByClassName("js-cd-hero");
+	if( heroSliders.length > 0 ) {
+		for( var i = 0; i < heroSliders.length; i++) {
+			(function(i){
+				new HeroSlider(heroSliders[i])
+			})(i);
+		}
+	}
+
+	//on mobile - open/close primary navigation clicking/tapping the menu icon 
+	document.getElementsByClassName('js-cd-header__nav')[0].addEventListener('click', function(event){
+		if(event.target.tagName.toLowerCase() == 'nav') {
+			classie.toggleClass(this.getElementsByTagName('ul')[0], 'cd-is-visible');
+		}
+	});
+
+	function removeClassPrefix(el, prefix) {
+		//remove all classes starting with 'prefix'
+        var classes = el.className.split(" ").filter(function(c) {
+            return c.indexOf(prefix) < 0;
+        });
+        el.className = classes.join(" ");
+	};
+
+	//class manipulations - needed if classList is not supported
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+		var classList = className.split(' ');
+	 	if (el.classList) el.classList.add(classList[0]);
+	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
+	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
+	}
+	function removeClass(el, className) {
+		var classList = className.split(' ');
+	  	if (el.classList) el.classList.remove(classList[0]);	
+	  	else if(hasClass(el, classList[0])) {
+	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+	  		el.className=el.className.replace(reg, ' ');
+	  	}
+	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
+	}
+})();
